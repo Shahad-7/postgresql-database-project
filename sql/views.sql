bdc6655aff49fe9563f6fb098ce1954ba549a20d
Shahad ALharbi <shahad.alharbi979@gmail.com>
2025-09-30 12:17:28 +0300

    Update views.sql

--- sql/views.sql
+++ sql/views.sql
@@ -6,3 +6,19 @@ SELECT
     position,
     location
 FROM staff;
+
+-- =========================================
+-- VIEW2 (merged)
+-- =========================================
+
+CREATE MATERIALIZED VIEW product_info_mview AS
+SELECT 
+    p.product_id,
+    p.product_name,
+    p.description,
+    p.price,
+    pt.product_category,
+    pt.product_type
+FROM product p
+JOIN product_type pt
+    ON p.product_type_id = pt.product_type_id;
